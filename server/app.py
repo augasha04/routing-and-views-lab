a2056324ae6302b733bf587f05d4ed9a2158c411
augasha04 <augustinemwangi04@gmail.com>
2023-06-20 15:42:12 +0300

    done with the lab, one one failling

--- server/app.py
+++ server/app.py
@@ -1,8 +1,33 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)
+    return param
+
+@app.route('/count/<int:param>')
+def count(param):
+    numbers = '\n'.join(str(num) for num in range(param + 1))
+    return numbers + '\n'
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    result = 0
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    return str(result)
